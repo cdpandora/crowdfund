7bb870e9ffd5e37182d1fad8a032bbd39d81d496
cdpandora <125117506+cdpandora@users.noreply.github.com>
2025-01-31 23:00:42 -0800

    Minor Improvements

--- src/fund.sol
+++ src/fund.sol
@@ -1,46 +1,128 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Fund {
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-// @dev Crowdfunding contract
+contract Fund is ReentrancyGuard {
+
+    // @dev Crowdfunding contract
 
     address owner;
     uint256 projectBalance = address(this).balance;
     uint256 target;
     uint256 deadline = block.timestamp + 7 days;
+    bool fundStart;
+    bool fundFailed;
     
     mapping(address => uint256) public donor;
+    
+    // @dev There are 3 teirs and depositors will be added based on 
+    // how much contributed (gold, plat, diamond)
+    // gold is 1 ether
+    // plat is 2 ether
+    // diamond is 4 ether
 
+    mapping(address => Teirs) public _Teirs;
+
+    event _deposit(uint256, string);
+    event _withdraw(string);
+    event _TeirUpgrade(Teirs);
+
+    enum Teirs {
+        None,
+        Gold,
+        Platinum,
+        Diamond
+    }
 
     constructor (uint256 _target) {
         owner = msg.sender;
         target = _target;
     }
 
-    event _deposit(uint256, string);
-    event _withdraw(string);
-
     modifier onlyOwner() {
         require(msg.sender == owner, "You do not have permission");
         _;
     }
 
-    function deposit() public payable {
+    function deposit() external payable nonReentrant {
+
+        address from = msg.sender;
+        uint256 value = msg.value;
+        // require target has not reached
         require(projectBalance < target, "Target reached");
+        
+        // dont allow deposits if they already have a teir
+        require(_Teirs[from] == Teirs.None , "Already In a Teir");
 
-        require(block.timestamp < deadline);
-        require(msg.value >= 1e18, "You have to deposit 1 eth");
-        projectBalance += msg.value;
 
-        emit _deposit(msg.value, "deposit successful");
-        donor[msg.sender] = msg.value;
+        require(block.timestamp < deadline, "Deadline Passed");
+        require(value >= 1e18, "You have to deposit at least 1 eth");
+
+
+
+        projectBalance += value;
+
+
+        // add to teirs
+        if (value >= 1e18 && value < 2e18 ) {
+            _Teirs[from] = Teirs.Gold;
+        } else if (value >= 2e18 && value < 4e18) {
+            _Teirs[from] = Teirs.Platinum;
+        } else {
+            _Teirs[from] = Teirs.Diamond;
+        }
+
+        donor[from] = value;
+        fundStart = true;
+        emit _deposit(value, "deposit successful");
     }
 
-    function withdraw(address _to) public onlyOwner{
+    function withdraw(address _to) external onlyOwner {
+       require(projectBalance == target, "target not reached!");
+        require(_to != address(0), "incorrect address");
+        require(fundStart);
+
         (bool success,) = _to.call{value: projectBalance}("");
         require(success, "Withdrawal failed");
+
         emit _withdraw("Withdrawal successfull");
     }
-    
+
+    function claimRefund(uint256 amount) external nonReentrant {
+        address _addr = msg.sender;
+        require(amount > 0 && donor[_addr] >= amount, "Not Enough Balance");
+        require(projectBalance < target, "Target reached");
+        
+        donor[_addr] -= amount;
+
+        (bool success,) = _addr.call{value: amount}("");
+        require(success);
+    }
+
+
+    function upgradeTeir() external payable nonReentrant {
+        address sender = msg.sender;
+        require(_Teirs[sender] != Teirs.Diamond, "Already Maxxed out");
+
+        uint256 balanceBefore = donor[sender];
+        uint256 balanceAfter = balanceBefore + msg.value;
+
+        require(balanceAfter >= 2e18, "oops! Stuck, Send at least 2ether to pass Gold Teir");
+        
+        if (balanceAfter >= 2e18) {
+            _Teirs[sender] = Teirs.Platinum;
+        } else if (balanceAfter >= 4e18) {
+            _Teirs[sender] = Teirs.Diamond;
+        }
+        
+        donor[sender] += msg.value;
+
+        emit _TeirUpgrade(_Teirs[sender]);
+    }
+
+    receive() external payable {
+        
+    }
+
 }
--- test/fund.t.sol
+++ test/fund.t.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
+import { Test, console } from "../lib/forge-std/src/Test.sol";
+
 import { Fund } from "../src/fund.sol";
 
 
@@ -10,9 +12,10 @@ contract testFund is Test {
     address Bob = payable(address(2));
     address James = payable(address(3));
     address owner = payable(address(0x1));
+    uint256 target = 10 ether;
 
     function setUp() public {
-        fund = new Fund(10 ether);
+        fund = new Fund(target);
     }
 
     function test_deposit() public {
@@ -67,9 +70,47 @@ contract testFund is Test {
         vm.prank(Bob);
         fund.deposit{value: 5 ether}();
 
-        vm.prank(James);
+        vm.startPrank(James);
         vm.expectRevert();
         fund.withdraw(owner);
+    }
+
+    function test_upgradeTeir() public {
+        vm.deal(Alice, 4 ether);
+        vm.startPrank(Alice);
+        // Deposit first to get Added to teir
+        fund.deposit{value: 1 ether}();
+
+        // Upgrade Teir
+        fund.upgradeTeir{value: 2 ether}();
+    }
+
+    function test_claimRefundBeforeTargetReached() public {
+        vm.deal(Alice, 4 ether);
+        vm.startPrank(Alice);
+        // Deposit first to get Added to teir
+        fund.deposit{value: 1 ether}();
+
+        fund.claimRefund(1 ether);
+    }
+
+    function test_claimRefundAfterTargetReached() public {
+        vm.deal(Alice, 4 ether);
+        vm.startPrank(Alice);
+        // Deposit first to get Added to teir
+        fund.deposit{value: 1 ether}();
+        vm.stopPrank();
+
+        vm.deal(Bob, 10 ether);
+        vm.prank(Bob);
+        // Target is 10 ether
+        fund.deposit{value: 9 ether}();      
+
+        vm.warp(block.timestamp + 10 days);
+
+        vm.prank(Alice);
+        vm.expectRevert();
+        fund.claimRefund(1 ether);
     }
 
 } 
